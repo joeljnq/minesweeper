f32c9481786426446c06ccfc2ddce99dac5365c1
JoelNQ <joelxdnq@gmail.com>
2023-04-13 09:58:33 +0200

    a

--- BuscMinas/src/buscaMinas/Main.java
+++ BuscMinas/src/buscaMinas/Main.java
@@ -28,6 +28,7 @@ public class Main {
                 System.out.println("Primero elige las filas, luego las columnas y por ultimo las minas que quieres que tenga el juego");
                 Xogo a = new Xogo(filas = teclado.nextInt(), columnas = teclado.nextInt(), minas = teclado.nextInt());
                 do {
+
                     a.muestra();
                     a.menuInterno();
                     eleccionMenuInterno = teclado.nextInt();
@@ -36,33 +37,35 @@ public class Main {
                         System.out.println("primero pon la fila de la celda que quieres abrir");
                         System.out.println("Luego pon la columna");
                         a.abrirCela(a.getCelda(filas = teclado.nextInt(), columnas = teclado.nextInt()));
-                        a.muestra();
                         System.out.println("\n");
 
-                        if (!finJuego) {
-                            a.menuInterno();
-                            eleccionMenuInterno = teclado.nextInt();
-                        }
-
                     } else if (eleccionMenuInterno == 2 && finJuego == false) {
-                        a.abrirTodasCelas();
-                        a.muestra();
+                        a.abrirTodasMinas();
 
-                        if (!finJuego) {
-                            a.menuInterno();
-                            eleccionMenuInterno = teclado.nextInt();
-                        }
                     } else if (eleccionMenuInterno == 3 && finJuego == false) {
+                        System.out.println(a.comprobarCelasAbrir());
                         System.out.println("False = aun queda Celdas por abrir");
                         System.out.println("True = todas las celdas estan abiertas.");
-                        System.out.println(a.comprobarCelasAbrir());
 
+                    }else if (eleccionMenuInterno == 4 && finJuego == false) {
+                        System.out.println("Primero pon la fila de la celda que quieres marcar");
+                        System.out.println("Luego la colummna");
+                        a.marcarCelda(a.getCelda(filas=teclado.nextInt(), columnas=teclado.nextInt()));
+                    }else if(eleccionMenuInterno == 5 && finJuego == false){
+                         System.out.println("Primero pon la fila de la celda que quieres marcar");
+                        System.out.println("Luego la colummna");
+                        System.out.println(a.getMinasAdxacentes(a.getCelda(filas=teclado.nextInt(), columnas=teclado.nextInt())));      
                     }
-                } while (eleccionMenuInterno != 4 && finJuego == false);
+
+                    if (a.comprobarCelasAbrir()) {
+                        System.out.println("Ganaste la partida");
+                        finJuego = true;
+                    }
+
+                } while (eleccionMenuInterno != 6 && finJuego == false);
 
             }
-        } while (eleccionMenuPrincipal
-                != 2);
+        } while (eleccionMenuPrincipal != 2);
 
     }
 }
--- BuscMinas/src/buscaMinas/Xogo.java
+++ BuscMinas/src/buscaMinas/Xogo.java
@@ -1,17 +1,20 @@
 package buscaMinas;
+
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+
 /**
  *
  * @author a18jaimejnq
  */
 public class Xogo {
-private Scanner teclado = new Scanner(System.in, "ISO-8859-1");
+
+    private Scanner teclado = new Scanner(System.in, "ISO-8859-1");
     private int filas;
     private int columnas;
     private Celda[][] mineSweeper;
-    private int minas ;
+    private int minas;
     Random random = new Random();
 
     public Xogo(int filas, int columnas, int minas) {
@@ -19,9 +22,8 @@ private Scanner teclado = new Scanner(System.in, "ISO-8859-1");
         int estado = 1;
         this.filas = filas;
         this.columnas = columnas;
-        this.minas= minas;
-        
-        
+        this.minas = minas;
+
         mineSweeper = new Celda[filas][columnas];
         for (int i = 0; i < mineSweeper.length; i++) {
             for (int j = 0; j < mineSweeper[i].length; j++) {
@@ -29,10 +31,8 @@ private Scanner teclado = new Scanner(System.in, "ISO-8859-1");
                 mineSweeper[i][j] = new Celda(false, estado, i, j);
             }
         }
-        
-        encherMinas(minas);
-       
 
+        encherMinas(minas);
     }
 
     /*public Celda[][] getCelda(int fila, int columna) {
@@ -40,18 +40,51 @@ private Scanner teclado = new Scanner(System.in, "ISO-8859-1");
   }
      */
     public void muestra() {
-        for (int i = 0; i < mineSweeper.length; i++) {
-            System.out.print("|");
+        /*   for (int i = 0; i < mineSweeper.length; i++) {
+            
             for (int j = 0; j < mineSweeper[i].length; j++) {
-                
-                
-                 System.out.print(mineSweeper[i][j].getEstado()+"|");
                
+                System.out.print("|" + mineSweeper[i][j].getEstado() + "|\t");
             }
-           
-          
             System.out.println("");
         }
+         */
+
+        Character[][] muestra = new Character[filas][columnas];
+
+        for (int i = 0; i < muestra.length; i++) {
+            System.out.print(i + "\t");
+
+            for (int j = 0; j < muestra[i].length; j++) {
+                
+                if (mineSweeper[i][j].getEstado() == 1) {
+                      System.out.print("X\t");
+                }else if (mineSweeper[i][j].getEstado() == 2) {
+                    System.out.print("T\t");
+                }else if (mineSweeper[i][j].getEstado() == 3) {
+                    if (mineSweeper[i][j].isMinada()) {
+                        System.out.print("*\t"); 
+                    }else{
+                        System.out.print(getMinasAdxacentes(mineSweeper[i][j])
+                         + "\t");
+                                
+                    }
+                    
+                }
+                
+              
+
+            }
+            System.out.println("");
+
+        }
+        System.out.println("");
+        for (int i = 0; i < muestra.length; i++) {
+            System.out.print("\t");
+             System.out.print(i);
+           
+        }
+
     }
 
     public Celda getCelda(int fila, int columna) {
@@ -59,79 +92,96 @@ private Scanner teclado = new Scanner(System.in, "ISO-8859-1");
     }
 
     public void abrirCela(Celda cela) {
+        //  boolean comprobrarCadxacentes = false; esto es un atributo para la recurisvidad 
         if (cela.getEstado() == 3) {
             System.out.println("Ya está destapada");
-        }else{
-            cela.setEstado(3);  
-        }
-      
-        
-    
-         //   System.out.println(getCelasAdxacentes(cela)+ "\n");
-        
-  
-        
-    }
-
-    public void abrirTodasCelas() {
-        for (int i = 0; i < mineSweeper.length; i++) {
-            for (int j = 0; j < mineSweeper[i].length; j++) {
-                mineSweeper[i][j].setEstado(3);
-            }
+        } else {
+            cela.setEstado(3);
         }
     }
-/* Este metodo es uno que cree yo para comprobar si todas las celas se abrieron
-    public void comprobarMinas() {
-        for (Celda[] celdas : mineSweeper) {
-            for (Celda celda : celdas) {
 
-                System.out.println(celda);
-            }
-        }
-    }*/
-    
-    public boolean comprobarCelasAbrir(){ // da false si hay celas sin abrir y true cuando todo esta abierto...
-       boolean toret = true;
+    public void abrirTodasMinas() {
         for (int i = 0; i < mineSweeper.length; i++) {
             for (int j = 0; j < mineSweeper[i].length; j++) {
-                if (mineSweeper[i][j].getEstado() != 3) {
-                    toret = false;
+                if (mineSweeper[i][j].isMinada()) {
+                    mineSweeper[i][j].setEstado(3);
                 }
+            }
+        }
+    }
+
+    public boolean comprobarCelasAbrir() { // da false si hay celas sin abrir y true cuando todo esta abierto...
+        boolean toret = true;
+        for (int i = 0; i < mineSweeper.length; i++) {
+            for (int j = 0; j < mineSweeper[i].length; j++) {
+                if (mineSweeper[i][j].isMinada() == false) {
+                    if (mineSweeper[i][j].getEstado() != 3) {
+                        toret = false;
+                    }
+                }
+
             }
         }
         return toret;
     }
-    
-    
-    private void encherMinas(int minas){
-        
-       
+
+    private void encherMinas(int minas) {
+
         for (int i = 0; i < minas; i++) {
             mineSweeper[random.nextInt(6)][random.nextInt(6)].setMinada(true);
         }
     }
-    
-    private ArrayList <Celda> getCelasAdxacentes(Celda cela){
+
+    public void marcarCelda(Celda celda) {
+        celda.setEstado(2);
+    }
+
+    private ArrayList<Celda> getCelasAdxacentes(Celda cela) {
         ArrayList<Celda> toret = new ArrayList<>();
-        toret.add(mineSweeper[cela.getFilas()][cela.getColumnas()+1]);
-        toret.add(mineSweeper[cela.getFilas()][cela.getColumnas()-1]);
-        toret.add(mineSweeper[cela.getFilas()+1][cela.getColumnas()]);
-        toret.add(mineSweeper[cela.getFilas()+1][cela.getColumnas()-1]);
-        toret.add(mineSweeper[cela.getFilas()+1][cela.getColumnas()+1]);
-        toret.add(mineSweeper[cela.getFilas()-1][cela.getColumnas()]);
-        toret.add(mineSweeper[cela.getFilas()-1][cela.getColumnas()+1]);
-        toret.add(mineSweeper[cela.getFilas()-1][cela.getColumnas()-1]);
+        /*   toret.add(mineSweeper[cela.getFilas()][cela.getColumnas() + 1]);
+        toret.add(mineSweeper[cela.getFilas()][cela.getColumnas() - 1]);
+        toret.add(mineSweeper[cela.getFilas() + 1][cela.getColumnas()]);
+        toret.add(mineSweeper[cela.getFilas() + 1][cela.getColumnas() - 1]);
+        toret.add(mineSweeper[cela.getFilas() + 1][cela.getColumnas() + 1]);
+        toret.add(mineSweeper[cela.getFilas() - 1][cela.getColumnas()]);
+        toret.add(mineSweeper[cela.getFilas() - 1][cela.getColumnas() + 1]);
+        toret.add(mineSweeper[cela.getFilas() - 1][cela.getColumnas() - 1]);*/
+
+        int filaInicial = cela.getFilas();
+        int columnaInicial = cela.getColumnas();
+
+        for (int f = filaInicial - 1; f <= filaInicial + 2; f++) {
+            for (int c = columnaInicial - 1; c <= columnaInicial + 2; c++) {
+                if (f != filaInicial) {
+                    toret.add(mineSweeper[f][c]);
+                }
+            }
+
+        }
         return toret;
     }
-    
-    public void menuInterno(){
-       
-        System.out.println("\n 1.abrir Celda");
-        System.out.println("2. Abrir todas las celdas");
-        System.out.println("3. Comprueba si hay celdas por abrir");
-        System.out.println("4. meter minas");
+
+    public int getMinasAdxacentes(Celda celda) {
+        int toret = 0;
+         for (int i = 0; i < getCelasAdxacentes(celda).size(); i++) {
+            if (getCelasAdxacentes(celda).get(i).isMinada()) {
+                toret++;
+            }
+        }
+         
+      /*  for (Celda celdaAdyacente : getCelasAdxacentes(celda)) {
+            if (celdaAdyacente.isMinada()) {
+                toret++;
+            }
+        }*/
+        return toret;
     }
 
- 
-
+    public void menuInterno() {
+        System.out.println("\n 1.abrir Celda");
+        System.out.println("2. Abrir todas las celdas que tienen minas");
+        System.out.println("3. Comprueba si hay celdas por abrir");
+        System.out.println("4. Marcar celda");
+        System.out.println("5. obtener las minas que hay alreder de tu celda");
+    }
 }
